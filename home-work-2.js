fa021c3a1794a42adc441490e45ca5853c511f06
root <q8x8l@yandex.ru>
2021-03-22 23:36:55 +0300

    first commit

---
+++ home-work-2.js
@@ -0,0 +1,23 @@
+// let m = prompt ('Введите массу тела')
+// let v = prompt ('Введите скорость тела')
+
+// let E = (m * Math.pow(v, 2)) / 2
+// console.log('Кинетическая энергия равна', E);
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+// console.log(a ** 2 + b ** 2);
+
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+// const c = Math.sqrt(a ** 2 + b ** 2)
+// console.log(c);
+
+// const S = prompt('S = ')
+// const t = prompt('t = ')
+// console.log('Скорость равна: ', S / t);
+
+const m = prompt('Масса = ')
+const c = 3 * 10 ** 8
+console.log('Энергия равна: ', m * c);
